e874eb75e3a3948fe225eecad79b98dacc3d0127
Aaron García <aargarcia@aidacanarias.com>
2022-10-13 09:03:21 +0100

    refactor: use test cases for kata steps example

--- LeapYearKata.Test/LeapYearsShould.cs
+++ LeapYearKata.Test/LeapYearsShould.cs
@@ -10,33 +10,34 @@ namespace LeapYearKata.Test {
                 leapYears = new LeapYears();
         }
 
-        [Test]
-        public void return_false_if_year_is_not_divisible_by_4() {
-            var year = 2017;
-
+        [TestCase(2017)]
+        [TestCase(2018)]
+        [TestCase(2019)]
+        public void return_false_if_year_is_not_divisible_by_4(int year) {
             var isLeapYear = leapYears.IsLeapYear(year);
 
             isLeapYear.Should().BeFalse();
         }
 
-        [Test]
-        public void return_true_if_year_is_divisible_by_4_but_not_by_100() {
-            var year = 2008;
-
+        [TestCase(2008)]
+        [TestCase(2012)]
+        [TestCase(2016)]
+        public void return_true_if_year_is_divisible_by_4_but_not_by_100(int year) {
             var isLeapYear = leapYears.IsLeapYear(year);
 
             isLeapYear.Should().BeTrue();
         }
-        
-        [Test]
-        public void return_false_if_year_is_divisible_by_100_but_not_by_400() {
-            var year = 1700;
 
+        [TestCase(1700)]
+        [TestCase(1800)]
+        [TestCase(1900)]
+        [TestCase(2100)]
+        public void return_false_if_year_is_divisible_by_100_but_not_by_400(int year) {
             var isLeapYear = leapYears.IsLeapYear(year);
 
             isLeapYear.Should().BeFalse();
         }
-        
+
         [Test]
         public void return_true_if_year_is_divisible_by_400() {
             var year = 2000;
