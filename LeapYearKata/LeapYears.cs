7afd13572b45fd818f864a023ca8638ad0b9c2ca
Aaron García <aargarcia@aidacanarias.com>
2022-10-13 08:52:04 +0100

    refactor : remove unnedded if

--- LeapYearKata/LeapYears.cs
+++ LeapYearKata/LeapYears.cs
@@ -2,8 +2,7 @@
 {
     public class LeapYears {
         public bool IsLeapYear(int year) {
-            if(year % 4 != 0) return false;
-            return true;
+            return year % 4 == 0;
         }
     }
 }
